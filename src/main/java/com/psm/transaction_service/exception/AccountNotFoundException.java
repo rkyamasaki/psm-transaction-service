18f9caf6962984aa9c8f39b3089b55ebd9c2b71d
Rubens Yamasaki <rkyamasaki@gmail.com>
2026-06-11 05:34:14 -0300

    - Creating testes for AccountService

---
+++ src/main/java/com/psm/transaction_service/exception/AccountNotFoundException.java
@@ -0,0 +1,8 @@
+package com.psm.transaction_service.exception;
+
+public class AccountNotFoundException extends BusinessException {
+
+    public AccountNotFoundException(Long accountId) {
+        super("Account with id %d".formatted(accountId));
+    }
+}
--- src/main/java/com/psm/transaction_service/service/AccountService.java
+++ src/main/java/com/psm/transaction_service/service/AccountService.java
@@ -3,9 +3,12 @@ package com.psm.transaction_service.service;
 import com.psm.transaction_service.domain.dto.AccountData;
 import com.psm.transaction_service.domain.entity.Account;
 import com.psm.transaction_service.exception.AccountAlreadyExistsException;
+import com.psm.transaction_service.exception.AccountNotFoundException;
 import com.psm.transaction_service.exception.InvalidDocumentException;
 import com.psm.transaction_service.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -32,7 +35,12 @@ public class AccountService {
         return accountRepository.save(new Account(documentNumber));
     }
 
-    public boolean isValidNumber(String value) {
+    public Account findAccountById(Long accountId) {
+        return accountRepository.findById(accountId)
+                .orElseThrow(() -> new AccountNotFoundException(accountId));
+    }
+
+    private boolean isValidNumber(String value) {
         return value != null
                 && value.matches("-?\\d+");
     }
