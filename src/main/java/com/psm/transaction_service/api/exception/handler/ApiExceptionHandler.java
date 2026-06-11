03940188e5e2162cae27835c5e0188e02d436d16
Rubens Yamasaki <rkyamasaki@gmail.com>
2026-06-11 13:14:30 -0300

    - Creating exception handler

---
+++ src/main/java/com/psm/transaction_service/api/exception/handler/ApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.psm.transaction_service.api.exception.handler;
+
+import com.psm.transaction_service.api.exception.response.BusinessErrorResponse;
+import com.psm.transaction_service.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<BusinessErrorResponse> handleBusinessException(
+            BusinessException exception
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new BusinessErrorResponse(exception.getMessage()));
+    }
+
+}
---
+++ src/main/java/com/psm/transaction_service/api/exception/response/BusinessErrorResponse.java
@@ -0,0 +1,6 @@
+package com.psm.transaction_service.api.exception.response;
+
+public record BusinessErrorResponse(
+        String errorMessage
+) {
+}
